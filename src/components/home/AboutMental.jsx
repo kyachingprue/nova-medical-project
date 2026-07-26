73e3fde7e9ee2885a61efa264363ff14bc73cc73
kyachingprue <kyachingpruemarma.info@gmail.com>
2026-07-26 22:14:09 +0600

    new code updated

--- src/components/home/AboutMental.jsx
+++ src/components/home/AboutMental.jsx
@@ -29,10 +29,13 @@ const AboutMental = () => {
         </div>
       </div>
       <motion.div
-        initial={{ opacity: 0, x: 50 }}
-        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, y: 80 }}
+        whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        transition={{ duration: 0.8 }}
+        transition={{
+          duration: 0.8,
+          ease: [0.22, 1, 0.36, 1] 
+        }}
         className="flex-1/2 w-full px-4"
       >
         {/* Section Title */}
